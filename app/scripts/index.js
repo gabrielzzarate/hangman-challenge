a0a281d753a7b5e60b1addf9692a9db7f8a7809e
Gabriel Zarate <gabrielzzarate@gmail.com>
2016-05-23 14:25:53 -0400

    add router and interface component

---
+++ app/scripts/index.js
@@ -0,0 +1,17 @@
+//3rd party
+window.jQuery = $ = require('jquery');
+var $ = require('jquery');
+var Backbone = require('backbone');
+var bootstrap = require('bootstrap-sass/assets/javascripts/bootstrap.js');
+
+
+//local
+var router = require('./router.js'); // app main router
+
+
+$(function(){
+
+Backbone.history.start();
+
+
+});
---
+++ app/scripts/router.js
@@ -0,0 +1,61 @@
+//3rd party
+var $ = require('jquery');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Interface = require("./components/Interface.jsx").Interface;
+
+
+
+
+var Router = Backbone.Router.extend({
+	_appMounted: false,
+
+	routes: {
+		"": "welcome",
+		"game/": "game",
+		"gameover/": "gameover",
+
+	},
+
+	login: function(){
+		ReactDOM.unmountComponentAtNode(appContainer);
+		ReactDOM.render(
+			React.createElement(Login, {router: self}),
+			appContainer
+		);
+
+		this._appMounted = false;
+	},
+	mainApp: function(route) {
+		var self = this;
+		self.current = route;
+
+		//Bail if the app component is already mounted
+		if(this._appMounted){
+			return;
+		};
+		ReactDOM.unmountComponentAtNode(appContainer);
+		ReactDOM.render(
+			React.createElement(Interface,
+				{router: self}),
+				appContainer);
+				this._appMounted = true;
+
+	},
+	welcome: function(){
+		this.mainApp("welcome");
+	},
+	game: function(){
+		this.mainApp("game");
+	},
+	gameover: function(){
+		this.mainApp("gameover");
+	}
+});
+
+module.exports = new Router();
+
+
